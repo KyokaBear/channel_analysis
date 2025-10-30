788ba7f6121ab4ecbdd95d1f98cd9918e5f6d5ef
Cynthia <2941346424@qq.com>
2025-10-30 11:54:15 +0800

    feat: Add base config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // --- 加上这行 ---
+  base: '/channel_analysis/', 
+  // ----------------
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
